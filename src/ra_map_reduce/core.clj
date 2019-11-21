c7c4deeca413d148620f0663106d57d19faef180
Trever Anderson <treveranderson01@gmail.com>
2019-11-21 17:49:06 -0600

    can read in records

--- src/ra_map_reduce/core.clj
+++ src/ra_map_reduce/core.clj
@@ -1,6 +1,16 @@
 (ns ra-map-reduce.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
+(defn records [in-str]
+  (let [[header & rows] (clojure.string/split-lines in-str)
+        h (str/split header #",")
+        rs (map #(str/split % #",") rows)]
+    (map (fn [r]
+           (map
+            (fn [k v] {(keyword k) v}) h r)) rs)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
