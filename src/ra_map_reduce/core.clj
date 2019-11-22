6393532420926a7d119477ff218916cfe485fa9d
Trever Anderson <treveranderson01@gmail.com>
2019-11-21 18:41:53 -0600

    fixed record func

--- src/ra_map_reduce/core.clj
+++ src/ra_map_reduce/core.clj
@@ -3,13 +3,22 @@
 
 (require '[clojure.string :as str])
 
-(defn records [in-str]
+(defn records [in-str table]
+  "Takes newline separated string with leading header and returns list of maps"
   (let [[header & rows] (clojure.string/split-lines in-str)
-        h (str/split header #",")
-        rs (map #(str/split % #",") rows)]
-    (map (fn [r]
-           (map
-            (fn [k v] {(keyword k) v}) h r)) rs)))
+        h (map keyword (str/split header #","))
+        rs (map #(vector %1 (str/split %2 #","))
+                (range 1 (count rows)) rows)]
+    (map
+     (fn [[o r]] (assoc (assoc
+                         (zipmap h r)
+                         :table table)
+                        :offset o)) rs)))
+
+;; (map-reduce team-data (fn [offset record] [1 record]) 1)
+;; (defn map-reduce [data map-func reduce-func]
+;;   (let [map-stage (map (fn [mr] (map-func 1 mr)) data)]
+;;     map-stage))
 
 (defn -main
   "I don't do a whole lot ... yet."
